5602653371cac8b8de4f673ecfd5703e5afd137b
ddiskandar <dd@smkplusalfarhan.sch.id>
2021-01-01 18:17:59 +0700

    data seed sekolah dan jurusan

--- database/seeders/SchoolSeeder.php
+++ database/seeders/SchoolSeeder.php
@@ -15,17 +15,340 @@ class SchoolSeeder extends Seeder
     public function run()
     {
         DB::table('schools')->insert([
-            'npsn' => '20277822',
-            'name' => 'MTs. YASTI 2 Kadudampit',
-            'address' => 'Cimahigirang',
-            'last_student' => 65,
+            'npsn' => '20277844',
+            'name' => 'MTS YASTI 2 KADUDAMPIT',
+            'address' => 'Sukabumi',
+            'last_student' => 36,
         ]);
 
         DB::table('schools')->insert([
-            'npsn' => '20268386',
+            'npsn' => '20268381',
             'name' => 'SMP NEGERI 2 KADUDAMPIT',
             'address' => 'Cibogo',
-            'last_student' => 76,
+            'last_student' => 48,
         ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '69976264',
+            'name' => 'AL-WASILAH 02',
+            'address' => 'Sukabumi',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277782',
+            'name' => 'MTs AL FALAH',
+            'address' => 'Sukabumi',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277761',
+            'name' => 'MTs AL HUDA CISAAT',
+            'address' => 'Cisaat',
+            'last_student' => 3,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277769',
+            'name' => 'MTs AL MUKHTARIYYAH',
+            'address' => 'Sukabumi',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '69928086',
+            'name' => 'MTs AL RAHIM',
+            'address' => 'Kadudampit',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '69955886',
+            'name' => 'MTs ALHIDAYAH CIBUNARKULON',
+            'address' => 'Kadudampit',
+            'last_student' => 4,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277826',
+            'name' => 'MTs KADUDAMPIT',
+            'address' => 'Kadudampit',
+            'last_student' => 5,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277683',
+            'name' => 'MTs NIDAUL KHOIR ( YASPIN )',
+            'address' => 'Pasir Datar',
+            'last_student' => 6,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277778',
+            'name' => 'MTs NURUL HUDA CISAAT',
+            'address' => 'Cisaat',
+            'last_student' => 2,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '69928085',
+            'name' => 'MTs RAUDLOTUL ULUM',
+            'address' => 'Citamiang',
+            'last_student' => 7,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277684',
+            'name' => 'MTs SA MUSLIMUN AL MUHSINIYAH',
+            'address' => 'Sukabumi',
+            'last_student' => 4,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277779',
+            'name' => 'MTs YAPIM JAYA',
+            'address' => 'Cisaat',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277772',
+            'name' => 'MTs YASTI 1 CISAAT',
+            'address' => 'Cisaat',
+            'last_student' => 4,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '69976307',
+            'name' => 'MTS. TAHFIZH AL FALAKIYAH',
+            'address' => 'Sukabumi',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277689',
+            'name' => 'MTSS AL - ANDALUSIA',
+            'address' => 'Sukabumi',
+            'last_student' => 3,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277689',
+            'name' => 'MTSS AL - ANDALUSIA',
+            'address' => 'Sukabumi',
+            'last_student' => 3,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277825',
+            'name' => 'MTSS AL - FURQON KAUDAMPIT',
+            'address' => 'Kebonkawung',
+            'last_student' => 27,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277688',
+            'name' => 'MTSS AL BADRIYAH',
+            'address' => 'Cisaat',
+            'last_student' => 4,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277771',
+            'name' => 'MTSS AL MASTHURIYAH',
+            'address' => 'Cisaat',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277824',
+            'name' => 'MTSS AT- TAHSISNIYYAH',
+            'address' => 'Sukabumi',
+            'last_student' => 2,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277773',
+            'name' => 'MTSS DAARUSSALAAM',
+            'address' => 'Sukabumi',
+            'last_student' => 5,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277774',
+            'name' => 'MTSS MIFTAHUL HUDA',
+            'address' => 'Sukabumi',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277685',
+            'name' => 'MTSS PLUS AS-SAIDAH',
+            'address' => 'Sukabumi',
+            'last_student' => 5,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277783',
+            'name' => 'MTSS TERPADU AL-IRFANIYYAH',
+            'address' => 'Sukabumi',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20277850',
+            'name' => 'MTSS TUNAS HARAPAN BANGSA',
+            'address' => 'Sukabumi',
+            'last_student' => 4,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20245289',
+            'name' => 'SMP DAARUL FALAAH CISAAT',
+            'address' => 'Cisaat',
+            'last_student' => 15,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20267734',
+            'name' => 'SMP ISLAM AL - ANHAR',
+            'address' => 'Cisaat',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20252035',
+            'name' => 'SMP ISLAM AL MAFTUH CARINGIN',
+            'address' => 'Caringin',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20253027',
+            'name' => 'SMP ISLAM PATRIA AL FADLAH',
+            'address' => 'Kadudampit',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20247079',
+            'name' => 'SMP ISLAM TERPADU YASPIDA',
+            'address' => 'Kadudampit',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20258063',
+            'name' => 'SMP IT AT-TAISIRIYAH',
+            'address' => 'Sukabumi',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20202384',
+            'name' => 'SMP NEGERI 1 CARINGIN',
+            'address' => 'Caringin',
+            'last_student' => 2,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20202417',
+            'name' => 'SMP NEGERI 1 CISAAT',
+            'address' => 'Cisaat',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20202409',
+            'name' => 'SMP NEGERI 1 GUNUNGGURUH',
+            'address' => 'Gunungguruh',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20202404',
+            'name' => 'SMP NEGERI 1 KADUDAMPIT',
+            'address' => 'Kadudampit',
+            'last_student' => 9,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20253294',
+            'name' => 'SMP NEGERI 2 BANTARGADUNG',
+            'address' => 'Bantargadung',
+            'last_student' => 2,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20202359',
+            'name' => 'SMP NEGERI 2 CISAAT',
+            'address' => 'Cisaat',
+            'last_student' => 4,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20202378',
+            'name' => 'SMP NEGERI 2 PABUARAN',
+            'address' => 'Pabuaran',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '69962072',
+            'name' => 'SMPN 6 CISOLOK SATU ATAP',
+            'address' => 'Cisolok',
+            'last_student' => 2,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20268796',
+            'name' => 'SMPN 7 SURADE SATU ATAP',
+            'address' => 'Surade',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20231139',
+            'name' => 'SMP PLUS ALHASANAH',
+            'address' => 'Cicurug',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20247058',
+            'name' => 'SMP PGRI BANTARGADUNG',
+            'address' => 'Bantargadung',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20220423',
+            'name' => 'SMP PGRI 2',
+            'address' => 'Sukabumi',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20233740',
+            'name' => 'SMP NEGERI SATU ATAP 1 LOSARANG',
+            'address' => 'Sukabumi',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '70000011',
+            'name' => 'SMP NEGERI 2 SERANG BARU',
+            'address' => 'Sukabumi',
+            'last_student' => 1,
+        ]);
+
+        DB::table('schools')->insert([
+            'npsn' => '20202378',
+            'name' => 'SMP NEGERI 2 PABUARAN',
+            'address' => 'Pabuaran',
+            'last_student' => 1,
+        ]);
+
     }
 }
