06620e1e7c3d31e3a508146adeaec9427a88afb0
ddiskandar <dd@smkplusalfarhan.sch.id>
2021-01-01 13:30:53 +0700

    Update pembayaran.blade.php

--- resources/views/admin/pembayaran.blade.php
+++ resources/views/admin/pembayaran.blade.php
@@ -9,7 +9,7 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're in master page
+                    You're in pembayaran page
                 </div>
             </div>
         </div>
