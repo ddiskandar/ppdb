6c243447f0680fedd9bf703f51f57c53c7584fb3
ddiskandar <dd@smkplusalfarhan.sch.id>
2021-01-04 06:37:08 +0700

    Update login-card.blade.php

--- resources/views/livewire/login-card.blade.php
+++ resources/views/livewire/login-card.blade.php
@@ -11,7 +11,7 @@
             </p>
         </div>
 
-        <x-input id="username" class="block w-full mt-3 placeholder-gray-400" type="text" name="username" :value="old('username')" placeholder="Nomor registrasi, misal : 2122xxxx" required autofocus />
+        <x-input id="username" class="block w-full mt-3 placeholder-gray-400" type="text" name="username" :value="old('username')" placeholder="Nomor registrasi, misal : 2122.0x.xxxx" required autofocus />
 
         <x-input id="password" class="block w-full mt-3 placeholder-gray-400" type="password" name="password" :value="old('password')" placeholder="Kata sandi" required autofocus />
 
