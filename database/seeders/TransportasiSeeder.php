eeeee7eaf43faddbe270935d6a54158a4deb78c2
ddiskandar <dd@smkplusalfarhan.sch.id>
2021-01-03 18:57:31 +0700

    tambah model referensi

---
+++ database/seeders/TransportasiSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TransportasiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
