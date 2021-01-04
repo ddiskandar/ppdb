84311ba165a1d7f37cb450e952274d2904125762
ddiskandar <dd@smkplusalfarhan.sch.id>
2021-01-04 11:04:52 +0700

    fix loading

--- resources/views/livewire/student/daftar-ppdb.blade.php
+++ resources/views/livewire/student/daftar-ppdb.blade.php
@@ -40,12 +40,17 @@
 
     @if ( ! Auth::user()->student->ppdb )
         <div class="py-6 text-center">
-            <form wire:submit.prevent="submitForm" action="/jalur" method="post">
+            <form wire:submit.prevent="submitForm" action="#" method="post">
             @csrf
             
-            <button type="submit" class="px-12 py-3 text-white bg-red-500 rounded-md hover:bg-red-600">
-                {{ __('Gabung')}}
+            <button type="submit" class="inline-flex justify-center py-3 text-sm font-medium text-white bg-red-500 border border-transparent rounded-md shadow-sm w-36 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-200">
+                <svg wire:loading wire:target="submitForm" class="w-5 h-5 -ml-1 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+                <span wire:loading.remove wire:target="submitForm">{{ __('Gabung')}}</span>
             </button>
+
             </form>
             
         </div>
