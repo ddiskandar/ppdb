cb4e4b15365904b032473b9260c3d7b933fc18b1
ddiskandar <dd@smkplusalfarhan.sch.id>
2021-01-28 19:23:31 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -33,7 +33,7 @@
         </div>
     </header>
 
-    <div class="py-4">
+    {{-- <div class="py-4">
         <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white">
                 <div class="flex items-center px-10 py-6 bg-red-200">
@@ -51,7 +51,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
     <div class="py-4">
         <div class="grid gap-6 px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 sm:grid-cols-2 md:grid-cols-4 ">
@@ -73,9 +73,11 @@
 
     <div class="py-6">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="grid md:grid-cols-12 md:gap-6">
+
+            <div class="md:grid md:grid-cols-12 md:gap-6">
+
                 <div class="md:col-span-7">
-                    <div class="flex flex-col">
+                    <div class="flex flex-col mb-4">
                         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                             <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
                                 <div class="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
