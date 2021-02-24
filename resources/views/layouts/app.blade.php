8ac2e1d2ecc3af63d41c16cb576663264c5a6cb4
ddiskandar <dd@smkplusalfarhan.sch.id>
2021-02-24 14:59:34 +0700

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,51 +1,54 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&display=swap">
-        
-        @livewireStyles
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&display=swap">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+    @livewireStyles
 
-        <style>
-            progress {
-                border-radius: 7px;
-            }
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-            progress::-webkit-progress-bar {
-                background-color: lightgrey;
-                border-radius: 7px;
-            }
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
 
-            progress::-webkit-progress-value {
-                background-color: green;
-                border-radius: 7px;
-            }
-        </style>
+    <style>
+        progress {
+            border-radius: 7px;
+        }
 
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+        progress::-webkit-progress-bar {
+            background-color: lightgrey;
+            border-radius: 7px;
+        }
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-        
-        @livewireScripts
+        progress::-webkit-progress-value {
+            background-color: green;
+            border-radius: 7px;
+        }
+    </style>
 
-    </body>
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+
+    @livewireScripts
+
+</body>
+
 </html>
